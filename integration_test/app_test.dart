56ab36e6ece88788af073e25539e79f6d522d1e3
Ranveer Singh Gour <31967062+RanveerSingh1997@users.noreply.github.com>
2022-07-23 19:23:34 +0530

    Revert "testing"

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_integration_testing/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 
-
-//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
-
-
 void main() {
   group("App Testing", () {
     testWidgets("Testing Widgets", (WidgetTester tester) async {
